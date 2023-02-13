43e25381c9ee53f61cfb0d359586c4ff6a6a18f2
Patryk Szaruga <szarugapatryk@gmail.com>
2023-02-13 21:34:05 +0100

    Add test for delete

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -10,8 +10,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -79,6 +78,19 @@ class StudentServiceTest {
 
     }
 
+    @Test
+    void canDeleteStudent() {
+        //given
+        given(studentRepository.existsById(anyLong()))
+                .willReturn(true);
+        long studentId = 1L;
+
+        //when
+        underTest.deleteStudent(studentId);
+        //then
+        verify(studentRepository).deleteById(studentId);
+
+    }
     @Test
     @Disabled
     void deleteStudent() {
